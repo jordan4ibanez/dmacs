7085948d9f41075b1dfd98d5b52819c588361cf1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-02 16:35:52 -0400

    HMMMM

--- source/app.d
+++ source/app.d
@@ -1,58 +1,88 @@
 import std.stdio;
 
-import gio.Application : GioApplication = Application;
-import gtk.Application;
-import gtk.ApplicationWindow;
-import gtk.Label;
-import gtk.ScrolledWindow;
-import gtk.TextBuffer;
-import gtk.TextView;
-import gtk.VBox;
+import dlangui;
 
-static final const class Dmacs {
-static:
-private:
+mixin APP_ENTRY_POINT;
 
-    ApplicationWindow masterWindow;
+/// entry point for dlangui based application
+extern (C) int UIAppMain(string[] args) {
 
-protected:
+    // load theme from file "theme_default.xml"
+    //Platform.instance.uiTheme = "theme_default";
 
-    void initialize(Application application) {
-        masterWindow = new ApplicationWindow(application);
+    // create window
 
-        masterWindow.setTitle("Dmacs");
-        masterWindow.setBorderWidth(10);
+    Window window = Platform.instance.createWindow("DlangUI example - HelloWorld", null);
 
-        // ScrolledWindow window = new ScrolledWindow();
+    // writeln(Platform.instance.uiTheme);
 
-        // window.setBorderWidth(10);
+    // window.mainWidget(new EditBox());
+    // window.mainWidget(new EditBox());
 
-        TextView view = new TextView();
+    writeln("hi");
 
-        view.setHscrollPolicy(GtkScrollablePolicy.NATURAL);
-        view.setVscrollPolicy(GtkScrollablePolicy.NATURAL);
+    // create some widget to show in window
+    //window.mainWidget = (new Button()).text("Hello, world!"d).margins(Rect(20,20,20,20));
+    // window.mainWidget = parseML(
+    //     q{
+    //     VerticalLayout {
+    //         margins: 10pt
+    //         padding: 10pt
+    //         layoutWidth: fill
+    //         // red bold text with size = 150% of base style size and font face Arial
+    //         TextWidget { text: "Hello World example for DlangUI"; textColor: "red"; fontSize: 150%; fontWeight: 800; fontFace: "Arial" }
+    //         // arrange controls as form - table with two columns
+    //         TableLayout {
+    //             colCount: 2
+    //             layoutWidth: fill
+    //             TextWidget { text: "param 1" }
+    //             EditLine { id: edit1; text: "some text"; layoutWidth: fill }
+    //             TextWidget { text: "param 2" }
+    //             EditLine { id: edit2; text: "some text for param2"; layoutWidth: fill }
+    //             TextWidget { text: "some radio buttons" }
+    //             // arrange some radio buttons vertically
+    //             VerticalLayout {
+    //                 layoutWidth: fill
+    //                 RadioButton { id: rb1; text: "Item 1" }
+    //                 RadioButton { id: rb2; text: "Item 2" }
+    //                 RadioButton { id: rb3; text: "Item 3" }
+    //             }
+    //             TextWidget { text: "and checkboxes" }
+    //             // arrange some checkboxes horizontally
+    //             HorizontalLayout {
+    //                 layoutWidth: fill
+    //                 CheckBox { id: cb1; text: "checkbox 1" }
+    //                 CheckBox { id: cb2; text: "checkbox 2" }
+    //                 ComboEdit { id: ce1; text: "some text"; minWidth: 20pt; items: ["Item 1", "Item 2", "Additional item"] }
+    //             }
+    //         }
+    //         EditBox { layoutWidth: 20pt; layoutHeight: 10pt }
+    //         HorizontalLayout {
+    //             Button { id: btnOk; text: "Ok" }
+    //             Button { id: btnCancel; text: "Cancel" }
+    //         }
+    //     }
+    //     }
+    // );
 
-        TextBuffer buf = view.getBuffer();
+    // you can access loaded items by id - e.g. to assign signal listeners
+    // auto edit1 = window.mainWidget.childById!EditLine("edit1");
+    // auto edit2 = window.mainWidget.childById!EditLine("edit2");
+    // // close window on Cancel button click
+    // window.mainWidget.childById!Button("btnCancel").click = delegate(Widget w) {
+    //     window.close();
+    //     return true;
+    // };
+    // // show message box with content of editors
+    // window.mainWidget.childById!Button("btnOk").click = delegate(Widget w) {
+    //     window.showMessageBox(UIString.fromRaw("Ok button pressed"d),
+    //         UIString.fromRaw("Editors content\nEdit1: "d ~ edit1.text ~ "\nEdit2: "d ~ edit2.text));
+    //     return true;
+    // };
 
-        // window.add(window);
+    // show window
+    window.show();
 
-        // window.add(new VBox(false, 0));
-
-        masterWindow.add(view);
-
-        masterWindow.showAll();
-    }
-
-public:
-
-}
-
-int main(string[] args) {
-    Application application = new Application("org.dmacs", GApplicationFlags.FLAGS_NONE);
-
-    application.addOnActivate(delegate void(GioApplication app) {
-        Dmacs.initialize(application);
-    });
-
-    return application.run(args);
+    // run message loop
+    return Platform.instance.enterMessageLoop();
 }
