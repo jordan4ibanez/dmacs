f368cbb403d0f29a38a7fb170bdeb22ebaa3c34f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-02 15:06:38 -0400

    So that's hello world

--- source/app.d
+++ source/app.d
@@ -1,5 +1,32 @@
 import std.stdio;
 
-void main() {
-    writeln("Edit source/app.d to start your project.");
+import gio.Application : GioApplication = Application;
+import gtk.Application;
+import gtk.ApplicationWindow;
+import gtk.Label;
+
+class HelloWorld : ApplicationWindow {
+
+    this(Application application) {
+
+        super(application);
+
+        setTitle("Dmacs");
+
+        setBorderWidth(10);
+
+        add(new Label("Hello World"));
+
+        showAll();
+    }
+
+}
+
+int main(string[] args) {
+    Application application = new Application("org.dmacs", GApplicationFlags.FLAGS_NONE);
+
+    application.addOnActivate(delegate void(GioApplication app) {
+        new HelloWorld(application);
+    });
+    return application.run(args);
 }
