339f15e9427a6310bb8756bb8bf3dc0f7e02b47c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-02 21:41:44 -0400

    IT'S WORKING AHH

--- source/app.d
+++ source/app.d
@@ -1,10 +1,14 @@
 import std.stdio;
 
+import gdk.Display;
+import gdk.MonitorG;
+import gdk.Rectangle;
 import gio.Application : GioApplication = Application;
 import gtk.Application;
 import gtk.ApplicationWindow;
 import gtk.Grid;
 import gtk.Label;
+import gtk.Paned;
 import gtk.ScrolledWindow;
 import gtk.TextBuffer;
 import gtk.TextView;
@@ -15,13 +19,22 @@ static:
 private:
 
     ApplicationWindow masterWindow;
-
-    
+    Display display;
+    MonitorG monitor;
 
 protected:
 
     void initialize(Application application) {
         masterWindow = new ApplicationWindow(application);
+        display = masterWindow.getDisplay();
+        monitor = display.getPrimaryMonitor();
+
+        { // Set the window to half the monitor size by default.
+            GdkRectangle rect;
+            monitor.getWorkarea(rect);
+            masterWindow.setSizeRequest(rect.width / 2, rect.height / 2);
+            masterWindow.setPosition(GtkWindowPosition.CENTER);
+        }
 
         masterWindow.setTitle("Dmacs");
         // masterWindow.setBorderWidth(4);
@@ -43,18 +56,18 @@ protected:
             scrollContainer.add(view);
         }
 
-        {
-            ScrolledWindow scrollContainer = new ScrolledWindow();
-            scrollContainer.setBorderWidth(4);
-            scrollContainer.setHexpand(true);
-            scrollContainer.setVexpand(true);
-            workArea.add(scrollContainer);
+        // {
+        //     ScrolledWindow scrollContainer = new ScrolledWindow();
+        //     scrollContainer.setBorderWidth(4);
+        //     scrollContainer.setHexpand(true);
+        //     scrollContainer.setVexpand(true);
+        //     workArea.add(scrollContainer);
 
-            TextView view = new TextView();
-            TextBuffer buf = view.getBuffer();
-            buf.setText("hi");
-            scrollContainer.add(view);
-        }
+        //     TextView view = new TextView();
+        //     TextBuffer buf = view.getBuffer();
+        //     buf.setText("hi");
+        //     scrollContainer.add(view);
+        // }
 
         // TextView view2 = new TextView();
         // TextBuffer buf2 = view2.getBuffer();
