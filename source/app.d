74c978a3d418aae411badf67bff36a8a07794a62
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-04 21:14:12 -0400

    I've had enough of gtk

--- source/app.d
+++ source/app.d
@@ -20,95 +20,9 @@ T instanceof(T)(Object o) if (is(T == class)) {
     return cast(T) o;
 }
 
-final class Module : Frame {
-    // Nothing to see here.
-    private static bool _____doom_____ = false;
-    private static bool _____doom2_____() {
-        const bool m = !_____doom_____;
-        if (!_____doom_____)
-            _____doom_____ = true;
-        return m;
-    }
-    // Move along.
-
-    const bool isMasterModule;
-
-    // This is to be kept in order.
-    Frame frame;
-    Paned pane;
-    ScrolledWindow scroll;
-    TextView text;
-
-    TextBuffer buf;
-
-    // The split.
-    Module child;
-
-    this(string buffer = "*scratch*") {
-        isMasterModule = _____doom2_____;
-        super(null, false);
-
-        // frame = new Frame();
-        // pane = new Paned(Orientation.Horizontal);
-        // scroll = new ScrolledWindow();
-        // text = new TextView();
-        // buf = Dmacs.getBuffer(buffer);
-
-        // text.setBuffer(buf);
-        // scroll.setChild(text);
-        // pane.setStartChild(scroll);
-        // frame.setChild(pane);
-
-        // frame.hexpandSet(true);
-        // pane.hexpandSet(true);
-        // scroll.hexpandSet(true);
-        // text.hexpandSet(true);
-
-        // frame.vexpandSet(true);
-        // pane.vexpandSet(true);
-        // scroll.vexpandSet(true);
-        // text.vexpandSet(true);
-
-        // pane.setResizeEndChild(true);
-        // pane.setResizeEndChild(true);
-
-        // frame.setHexpand(true);
-        // pane.setHexpand(true);
-        // scroll.setHexpand(true);
-        // text.setHexpand(true);
-
-        // frame.setVexpand(true);
-        // pane.setVexpand(true);
-        // scroll.setVexpand(true);
-        // text.setVexpand(true);
-
-        // const pixels = 50;
-
-        frame.setSizeRequest(50, 50);
-
-        if (isMasterModule) {
-            // Dmacs.masterModule = this;
-            // Dmacs.masterFrame.setChild(this.frame);
-        }
-    }
-
-    void splitRight(Module newMod) {
-        child = newMod;
-        // pane.setOrientation(orientation);
-        // pane.setEndChild(child.frame);
-
-        // pane.setResizeStartChild(true);
-        // pane.setResizeEndChild(true);
-
-        // pane.setShrinkStartChild(true);
-        // pane.setShrinkEndChild(true);
-    }
-
-}
-
 static final const class Dmacs {
 static:
-private:
+protected:
 
     /// This gets triggered if you ctrl+c Dmacs in the terminal.
     __gshared bool __DMACS_DIE_NOW;
@@ -128,8 +42,6 @@ private:
     /// Instead of thinking that you are editing a file, think that you are editing text in a buffer. When you save the buffer, the file is updated to reflect your edits. 
     TextBuffer[string] buffers;
     string[TextBuffer] bufferNameLookup;
-
-protected:
 
     int initialize(string[] args) {
         app = new Application("org.dmacs", GApplicationFlags.FLAGS_NONE);
@@ -177,6 +89,10 @@ protected:
         deploy();
     }
 
+    void deploy() {
+
+    }
+
     extern (C) void __terminationHandler(int _) nothrow @nogc {
         __DMACS_DIE_NOW.atomicStore(true);
     }
@@ -186,10 +102,6 @@ protected:
         write("\033[K\rThank you for using Dmacs.");
         app.quit();
         return true;
-    }
-
-    void deploy() {
-
     }
 
 public:
