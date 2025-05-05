c9243dec7f3e78a4706d55ff1815e4c98b78e88f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-05 18:21:18 -0400

    Hide Raylib in here

---
+++ source/dmacs/dumpster/dumpster.d
@@ -0,0 +1,3 @@
+module dmacs.dumpster.dumpster;
+
+public import raylib : ClearWindowState, ConfigFlags, GetCurrentMonitor, GetMonitorHeight, GetMonitorPosition, GetMonitorWidth, InitWindow, SetConfigFlags, SetWindowPosition, SetWindowSize, SetWindowTitle, Vector2, WindowShouldClose; // @suppress(dscanner.style.long_line)
