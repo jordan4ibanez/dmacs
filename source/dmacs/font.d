df1d553c4d9c83b22e140023ee849ff561f4b941
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-05 19:18:53 -0400

    Incomprehensible gibberish

--- source/dmacs/font.d
+++ source/dmacs/font.d
@@ -1,18 +1,30 @@
 module dmacs.font;
 
-import raylib : FontStruct = Font;
+import raylib : FontStruct = Font, LF = LoadFontEx, STF = SetTextureFilter, TF = TextureFilter;
 import std.stdio;
+import std.string;
 
 static final const package class Font {
 static:
-package:
+private:
 
     FontStruct[string] db;
     const dstring codePointAsciiString;
 
+    FontStruct loadFont(string location) {
+        auto l = LF(location.toStringz, 64, cast(int*) codePointAsciiString, 0);
+        STF(l.texture, TF.TEXTURE_FILTER_ANISOTROPIC_16X);
+        return l;
+    }
+
+package:
+
     void __initialize() {
+        if (codePointAsciiString.length != 0)
+            throw new Error("Do not init twice");
         foreach (char i; 0 .. 256)
             cast(dstring) codePointAsciiString ~= i;
+        db["default"] = loadFont("fonts/IosevkaTerm-Regular.ttf");
     }
 
 }
