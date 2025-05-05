160e0f0c390158f87d4153955f390946c861e64c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-04 20:32:08 -0400

    bring back

--- source/app.d
+++ source/app.d
@@ -6,11 +6,98 @@ import gtk.Paned;
 import gtk.TextBuffer;
 import gtk.TextView;
 import std.stdio;
+import gtk.ScrolledWindow;
 
 /// Check if an object is an instance of a class.
 pragma(inline, true)
 T instanceof(T)(Object o) if (is(T == class)) {
     return cast(T) o;
+}
+
+final class Module : Frame {
+    // Nothing to see here.
+    private static bool _____doom_____ = false;
+    private static bool _____doom2_____() {
+        const bool m = !_____doom_____;
+        if (!_____doom_____)
+            _____doom_____ = true;
+        return m;
+    }
+    // Move along.
+
+    const bool isMasterModule;
+
+    // This is to be kept in order.
+    Frame frame;
+    Paned pane;
+    ScrolledWindow scroll;
+    TextView text;
+
+    TextBuffer buf;
+
+    // The split.
+    Module child;
+
+    this(string buffer = "*scratch*") {
+        isMasterModule = _____doom2_____;
+        super(null, false);
+
+        // frame = new Frame();
+        // pane = new Paned(Orientation.Horizontal);
+        // scroll = new ScrolledWindow();
+        // text = new TextView();
+        // buf = Dmacs.getBuffer(buffer);
+
+        // text.setBuffer(buf);
+        // scroll.setChild(text);
+        // pane.setStartChild(scroll);
+        // frame.setChild(pane);
+
+        // frame.hexpandSet(true);
+        // pane.hexpandSet(true);
+        // scroll.hexpandSet(true);
+        // text.hexpandSet(true);
+
+        // frame.vexpandSet(true);
+        // pane.vexpandSet(true);
+        // scroll.vexpandSet(true);
+        // text.vexpandSet(true);
+
+        // pane.setResizeEndChild(true);
+        // pane.setResizeEndChild(true);
+
+        // frame.setHexpand(true);
+        // pane.setHexpand(true);
+        // scroll.setHexpand(true);
+        // text.setHexpand(true);
+
+        // frame.setVexpand(true);
+        // pane.setVexpand(true);
+        // scroll.setVexpand(true);
+        // text.setVexpand(true);
+
+        // const pixels = 50;
+
+        frame.setSizeRequest(50, 50);
+
+        if (isMasterModule) {
+            // Dmacs.masterModule = this;
+            // Dmacs.masterFrame.setChild(this.frame);
+        }
+    }
+
+    void splitRight(Module newMod) {
+        child = newMod;
+        // pane.setOrientation(orientation);
+        // pane.setEndChild(child.frame);
+
+        // pane.setResizeStartChild(true);
+        // pane.setResizeEndChild(true);
+
+        // pane.setShrinkStartChild(true);
+        // pane.setShrinkEndChild(true);
+    }
+
 }
 
 static final const class Dmacs {
@@ -33,6 +120,8 @@ public:
 
     void onActivate(GioApplication _) {
         writeln("hi");
+
+        win.add(new Frame(null, false));
 
         win.showAll();
     }
