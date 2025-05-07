65f780f8da748bade7e2bbbc92ed9ac644131e88
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-06 23:15:22 -0400

    Noice

--- source/dmacs/font.d
+++ source/dmacs/font.d
@@ -5,30 +5,37 @@ import raylib : Color, Colors, DrawTextEx, FontStruct = Font, GCP = GetCodepoint
 import std.stdio;
 import std.string;
 
+struct FontType {
+    FontStruct f;
+    int w = 0;
+    int h = 0;
+}
+
 static final const package class Font {
 static:
 private:
 
-    FontStruct[string] db;
+    FontType[string] db;
     const dstring codePointAsciiString;
-    char[1] __drawc;
+    char[2] __drawc;
+    FontType* curFont;
 
     // I wrote this like true elisp. It's just basic stuff, won't be doing this again lol.
 
     /// Get the size of a character in a font.
-    float getCharWidth(FontStruct* f, char c) {
+    int getCharWidth(FontStruct* f, char c) {
         // Vector2 w = MeasureTextEx(*f, [c].toStringz, 64, 0);
         // writeln(z, " <", c, "> ", w.x);
         return *((cast(int*)(f.glyphs + GGI(*f, GCP([c].toStringz, new int(0))))) + 3);
     }
 
-    /// Load up a font.
-    Option!FontStruct loadFont(string location) {
+    /// Load up a TTF font.
+    Option!FontStruct __loadTTF(string location) {
         auto l = LF(location.toStringz, 64, cast(int*) codePointAsciiString, 0);
         Option!FontStruct r;
         bool i = IFV(l);
         if (!i)
-            writeln("Font " ~ location ~ " is not a font.");
+            writeln("Font [" ~ location ~ "] is not a TTF font.");
         if (!i)
             return r;
         alias g = getCharWidth;
@@ -44,29 +51,35 @@ private:
         return r.Some(l);
     }
 
+    /// Process TTF.
+    Option!FontType loadFont(string location) {
+        Option!FontType res;
+        Option!FontStruct f = __loadTTF(location);
+        if (f.isNone)
+            return res;
+        FontType ft;
+        ft.f = f.unwrap;
+        ft.w = getCharWidth(&ft.f, 'a');
+        ft.h = ft.f.baseSize;
+        return res.Some(ft);
+    }
+
 package:
 
     void __initialize() {
+        __drawc[1] = '\0';
         if (codePointAsciiString.length != 0)
             throw new Error("Do not init twice");
         foreach (int i; 32 .. 127)
             cast(dstring) codePointAsciiString ~= cast(char) i;
         db["default"] = loadFont("fonts/IosevkaTerm-Regular.ttf").expect(
             "Please put the default font back.");
-        // loadFont("fonts/Ubuntu-Light.ttf");
+        curFont = "default" in db;
     }
 
     void drawChar(string font, char c) {
-        FontStruct f;
-        if (font in db) {
-            f = db[font];
-        } else {
-            writeln("font: " ~ font ~ " doesn't exist, defaulting");
-            f = db["default"];
-        }
         __drawc[0] = c;
-        DrawTextEx(f, __drawc.ptr, Vector2(0, 0), 64, 0, Colors.WHITE);
-
+        DrawTextEx(curFont.f, __drawc.ptr, Vector2(0, 0), 64, 0, Colors.WHITE);
     }
 
 }
