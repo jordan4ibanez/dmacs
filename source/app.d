65303a5bfa27b36b8b054bc2172f67ba255f4fa0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-07 01:23:32 -0400

    I see

--- source/app.d
+++ source/app.d
@@ -1,2 +1,45 @@
+import guino;
+import std.stdio;
+
+static final const class Dmacs {
+static:
+private:
+
+    bool g = false;
+    WebView wv;
+
+public:
+
+    void __initialize() {
+        if (g) {
+            throw new Error("initialized twice.");
+        }
+        g = true;
+        wv = WebView(true);
+        wv.html("<html><head><script>onload=jsMain();</script></head><body></body></html>");
+        wv.bindJs!jsMain;
+        wv.run();
+    }
+
+    void jsMain() {
+        writeln("hi!");
+
+        setBackgroundColor("black");
+    }
+
+    string quote(string input) {
+        return "\"" ~ input ~ "\"";
+    }
+
+    void runJS(string input) {
+        wv.eval(input ~ ";");
+    }
+
+    void setBackgroundColor(string color) {
+        runJS("document.body.style.background = " ~ quote(color));
+    }
+}
+
 void main(string[] args) {
+    Dmacs.__initialize();
 }
