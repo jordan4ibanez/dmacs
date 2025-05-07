b31409706af194c64f7fe6670ac3da442d0fd91b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-06 20:16:21 -0400

    Update dmacs_dumpster.d

--- source/dmacs/dumpster/dmacs_dumpster.d
+++ source/dmacs/dumpster/dmacs_dumpster.d
@@ -1,3 +1,3 @@
 module dmacs.dumpster.dmacs_dumpster;
 
-public import raylib : ClearWindowState, ConfigFlags, GetCurrentMonitor, GetMonitorHeight, GetMonitorPosition, GetMonitorWidth, InitWindow, SetConfigFlags, SetWindowPosition, SetWindowSize, SetWindowTitle, Vector2, WindowShouldClose; // @suppress(dscanner.style.long_line)
+public import raylib : ClearWindowState, ConfigFlags, GetCurrentMonitor, GetMonitorHeight, GetMonitorPosition, GetMonitorWidth, InitWindow, SetConfigFlags, SetTraceLogLevel, SetWindowPosition, SetWindowSize, SetWindowTitle, TraceLogLevel, Vector2, WindowShouldClose; // @suppress(dscanner.style.long_line)
