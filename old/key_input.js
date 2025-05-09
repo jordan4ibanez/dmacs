575f40709ca77f79f3a8d3001c3a6d7dba5a059a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-09 09:49:05 -0400

    Update key_input.js

--- old/key_input.js
+++ old/key_input.js
@@ -1,30 +1,10 @@
 const KeyInput = new (function () {
 	
 
-	/**
-	 * What happens when a key is pressed.
-	 * @param {KeyboardEvent} keyPressEvent
-	 */
-	function onKeyDown(keyPressEvent) {
-		// const key = keyPressEvent.key; // "a", "1", "Shift", etc.
-		for (var [_, v] of keyDownListenerEvents) {
-			v(keyPressEvent);
-		}
-	}
-
-	/**
-	 *  What happens when a key is released.
-	 * @param {KeyboardEvent} keyReleaseEvent
-	 */
-	function onKeyUp(keyReleaseEvent) {
-		// const key = keyReleaseEvent.key; // "a", "1", "Shift", etc.
-		// print(key, "UP");
-	}
 
 	
 
 	Init.deployFunction(() => {
-		document.addEventListener("keydown", onKeyDown);
-		document.addEventListener("keyup", onKeyUp);
+		
 	});
 })();
