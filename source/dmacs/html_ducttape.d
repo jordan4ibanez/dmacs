363b52dece31c2d04c20baa6d24d84dc5dddfbd6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-09 07:07:15 -0400

    Put this anomaly over here

--- source/dmacs/html_ducttape.d
+++ source/dmacs/html_ducttape.d
@@ -52,6 +52,16 @@ private:
 
 public:
 
+    // I do not feel like typing console.log over and over.
+    // I have NO idea how the D compiler is working out these symbols but this is great.
+    void writeln(JSONValue[] data) {
+        write("[js]: ");
+        foreach (JSONValue e; data) {
+            write(e, " ");
+        }
+        write("\n");
+    }
+
     void ductape() {
         __scriptify();
         __htmlify();
