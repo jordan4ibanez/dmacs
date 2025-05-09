7566374535a371eb53200509a5347402b609fc06
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-09 09:49:01 -0400

    Noice

--- ts/key_input.ts
+++ ts/key_input.ts
@@ -1,4 +1,32 @@
+import * as Init from "./init";
+Init.deploy("KeyInput", () => {
+	document.addEventListener("keydown", onKeyDown);
+	document.addEventListener("keyup", onKeyUp);
+});
+
+//? BEGIN IMPLEMENTATION.
+
 const keyDownListenerEvents = new Map();
+
+/**
+ * What happens when a key is pressed.
+ * @param {KeyboardEvent} keyPressEvent
+ */
+function onKeyDown(keyPressEvent: KeyboardEvent) {
+	// const key = keyPressEvent.key; // "a", "1", "Shift", etc.
+	for (var [_, v] of keyDownListenerEvents) {
+		v(keyPressEvent);
+	}
+}
+
+/**
+ *  What happens when a key is released.
+ * @param {KeyboardEvent} keyReleaseEvent
+ */
+function onKeyUp(keyReleaseEvent: KeyboardEvent) {
+	// const key = keyReleaseEvent.key; // "a", "1", "Shift", etc.
+	// print(key, "UP");
+}
 
 /**
  * Set a listener for key presses. If it exists, it will overwrite it.
