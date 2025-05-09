330b3c2c551a6f4a37541d8094e6722b1d40fbc8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-09 12:36:06 -0400

    Gut it

--- ts/init.ts
+++ ts/init.ts
@@ -1,28 +1,9 @@
-/**
- * Used for module initialization control.
- */
-class ModInit {
-	locked: boolean = false;
-	fn: () => void;
-	constructor(fn: () => void) {
-		this.fn = fn;
-	}
-}
-
-var mods: Map<string, ModInit> = new Map();
-
-/**
- * Use this to initialize a module when the program opens.
- * @param moduleName The name of the module.
- * @param safety An object used to ensure that modules are not initialized multiple times.
- * @param fn The function to run.
- */
-export function deploy(moduleName: string, fn: () => void) {
-	if (mods.has(moduleName)) {
-		throw new Error("Module " + moduleName + " was already defined.");
-	}
-	mods.set(moduleName, new ModInit(fn));
-}
+import * as Buffer from "./buffer";
+import * as Chord from "./chord";
+import * as CSS from "./css";
+import * as MiniBuffer from "./mini_buffer";
+import * as WindowControl from "./window_control";
+import * as KeyInput from "./key_input";
 
 var __deployed: boolean = false;
 /**
@@ -34,13 +15,7 @@ export function z____run() {
 	}
 	__deployed = true;
 
-	for (let [moduleName, mod] of mods) {
-		if (mod.locked) {
-			throw new Error(
-				"Mod [" + moduleName + "] was initialized twice, somehow."
-			);
-		}
-		mod.locked = true;
-		mod.fn();
-	}
+	Chord.z____deploy();
+	WindowControl.z____deploy();
+	KeyInput.z____deploy();
 }
