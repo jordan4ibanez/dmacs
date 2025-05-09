de31392817c13f133b6f91d89385ac3cbcdb18b8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-09 09:17:09 -0400

    Let's approach this with TS focused impl

--- ts/module_deploy.ts
+++ ts/module_deploy.ts
@@ -1,11 +1,35 @@
-import * as CSS from "./css";
-
 var __deployed: boolean = false;
 
+class Safety {
+	protected tick: boolean = false;
+}
+
+//? These are synchronized.
+var fns: (() => void)[] = [];
+var saf: Safety[] = [];
+
+/**
+ *
+ * @param fn You pass this function a blank object for
+ */
+export function add(safety: Safety, fn: () => void) {
+	fns.push(fn);
+	if (!safety) {
+		throw new Error("Safety was null.");
+	}
+	saf.push(safety);
+}
+
+/**
+ * Deploys all the modules to load up at initialization.
+ */
 export function run() {
 	if (__deployed) {
 		throw new Error("Deploy run twice.");
 	}
 	__deployed = true;
-	CSS.____deploy();
+
+	fns.forEach((fn: () => void) => {
+		fn();
+	});
 }
