9ab03b162a934cc69db264aeb240fc03e20efee1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-09 10:30:33 -0400

    Update buffer.ts

--- ts/buffer.ts
+++ ts/buffer.ts
@@ -1,3 +1,18 @@
 var focusedBuffer: string | null = null;
 
+/**
+ * Set the currently focused buffer.
+ * @param bufferName The name of the buffer.
+ */
+export function setFocus(bufferName: string): void {
+	focusedBuffer = bufferName;
+	// Chord.exitRecord(); // todo: fix this
+}
 
+/**
+ * Get the currently focused buffer.
+ * @returns The currently focused buffer. Or null.
+ */
+export function getFocus(): string | null {
+    return focusedBuffer;
+}
