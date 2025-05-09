558810297e2a7836c3690647a73f3b2f9f975787
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-09 08:27:12 -0400

    This is really bad lol

--- ts/css.ts
+++ ts/css.ts
@@ -12,7 +12,7 @@ export function ____deploy() {
 	document.head.appendChild(styleSheet);
 }
 
-var cssClassContainer: Map<string, string> = new Map();
+var cssClassContainer: Object = new Object();
 
 /**
  * Updates the CSS of the page.
@@ -21,7 +21,6 @@ export function rebuild() {
 	var rebuild: string[] = [];
 
 	rebuild.push("\n");
-
 
 	for (let [clazz, component] of Object.entries(cssClassContainer)) {
 		if (clazz === "*") {
