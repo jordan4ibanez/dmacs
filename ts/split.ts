b9589622f8efa1e7971f7fef29fa886474d8aae7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-10 00:32:56 -0400

    Explain this in the readme, don't load this into a browser

--- ts/split.ts
+++ ts/split.ts
@@ -3,10 +3,6 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// The programming goals of Split.js are to deliver readable, understandable and
-// maintainable code, while at the same time manually optimizing for tiny minified file size,
-// browser compatibility without additional requirements
-// and very few assumptions about the user's page layout.
 if (!window) {
 	throw new Error("No window.");
 }
