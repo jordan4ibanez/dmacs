3cec1a3cd3602ea696ef32e46c029872999d3d45
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-05-12 03:30:02 -0400

    Hello webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './ts/main.ts',
+  mode: "development",
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        include: path.resolve(__dirname, "ts"),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+};
